87ee5d044481477e8e3d76a4ef1151ae62f60325
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2026-03-28 10:18:56 +0300

    fix: a little better view for Canon CP1500 prints

--- src/const.py
+++ src/const.py
@@ -1,8 +1,10 @@
 # Image size in px is around cm in 300 dpi for Canon CP1500.
-CANVAS_WIDTH = 880      # 88 cm original
-CANVAS_HEIGHT = 1070    # 107 cm original
-BORDER_SIZE = 30        # 0.5 cm original
-CAPTION_SIZE = 120      # 2.8 cm original
+
+
+CANVAS_WIDTH = 880      # 88 mm original
+CANVAS_HEIGHT = 1070    # 107 mm original
+BORDER_SIZE = 40        # 5 mm original
+CAPTION_SIZE = 120      # 28 mm original
 
 SYSTEM_FONT = 'Courier New Bold.ttf'
 FONT_SIZE = 30
