9d1a76bb8e44028ba0e23e9876b4e863441858f0
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2026-03-28 10:28:16 +0300

    style: borders sizes for Canon CP1500 printing

--- src/const.py
+++ src/const.py
@@ -3,7 +3,8 @@
 
 CANVAS_WIDTH = 880      # 88 mm original
 CANVAS_HEIGHT = 1070    # 107 mm original
-BORDER_SIZE = 40        # 5 mm original
+BORDER_W_SIZE = 100     # Canon CP1500 2 photos on an one paper
+BORDER_H_SIZE = 40      # 5 mm original
 CAPTION_SIZE = 120      # 28 mm original
 
 SYSTEM_FONT = 'Courier New Bold.ttf'
